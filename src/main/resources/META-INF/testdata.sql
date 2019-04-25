b825f2a3714f8ed3b2e28eb638abe3fbaf09434b
mf226 <cph-mf226@cphbusiness.dk>
2019-04-25 09:35:52 +0200

    added hashing/saltet pw's

--- src/main/resources/META-INF/testdata.sql
+++ src/main/resources/META-INF/testdata.sql
@@ -1,6 +1,6 @@
 USE seed;
-INSERT INTO users (user_name, user_pass) VALUES ('user', 'test');
-INSERT INTO users (user_name, user_pass) VALUES ('admin', 'test');
+INSERT INTO users (user_name, user_pass) VALUES ('user', '$2a$10$mSZjeQg//BMfLL4Qpf0wwO9fuS3nqUE2GB.gNcRfRBRUgiSTX8Vwm');
+INSERT INTO users (user_name, user_pass) VALUES ('admin', '$2a$10$E1GGASKVGmZvQK9Pkyh4sObxHR3x1hnpp7qTQwJDotDBOWZwBLy7a');
 INSERT INTO roles (role_name) VALUES ('user');
 INSERT INTO roles (role_name) VALUES ('admin');
 INSERT INTO user_roles (user_name, role_name) VALUES ('user', 'admin');
