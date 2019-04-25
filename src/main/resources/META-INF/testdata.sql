e995bc7c38384cc8df8f2befab5c213d1b48fd03
mf226 <cph-mf226@cphbusiness.dk>
2019-04-25 10:28:35 +0200

    final

--- src/main/resources/META-INF/testdata.sql
+++ src/main/resources/META-INF/testdata.sql
@@ -1,4 +1,10 @@
 USE seed;
+
+-- Delete all previous data first
+DELETE FROM user_roles WHERE NOT user_name='xxx';
+DELETE FROM roles WHERE NOT role_name ='xxx';
+DELETE FROM users WHERE NOT user_name ='xxx';
+
 INSERT INTO users (user_name, user_pass) VALUES ('user', '$2a$10$mSZjeQg//BMfLL4Qpf0wwO9fuS3nqUE2GB.gNcRfRBRUgiSTX8Vwm');
 INSERT INTO users (user_name, user_pass) VALUES ('admin', '$2a$10$E1GGASKVGmZvQK9Pkyh4sObxHR3x1hnpp7qTQwJDotDBOWZwBLy7a');
 INSERT INTO roles (role_name) VALUES ('user');
